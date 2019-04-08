33ddf806bff03fe45d931d275c76e34be75bbfed
syn <vicdemilly@gmail.com>
2019-04-08 11:41:31 +0200

    Création d'une boite de dialogue à trois variables, non stylisé

---
+++ main.js
@@ -0,0 +1,4 @@
+const prenom = prompt("Quel est votre prénom ?");
+const nom = prompt("Quel est votre nom ?");
+const ville = prompt("De quelle ville venez-vous ?")
+alert(`Prénom : ${prenom} \n Nom : ${nom} \n Ville : ${ville}`)
